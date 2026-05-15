04c59e0c4842e34e8dbededeac2619ed396686b9
Jankatay <barjanay@proton.me>
2026-05-15 04:35:03 -0500

    made major milestone in cgi

--- site-attempt/site/cgi-bin/main.pl
+++ site-attempt/site/cgi-bin/main.pl
@@ -3,11 +3,22 @@
 use warnings;
 use strict;
 
-print("Content-type: video/mp4\n");
-print("Content-Disposition: attachment\n\n"); 
+# HTTP headers
+my $boundary = "Ucmd3AW\$|o-!2.<-<!:<~~-`ST-98o+-}`o-%[2ix-|\$nA\"-:yx?";
+print "Content-type: video/mp4\r\n";
+print "Content-Disposition: attachment\r\n";
+print "Content-Type: multipart/form-data; boundary=$boundary\r\n\r\n";
 
-my $filename = "./video.mp4";
-open(FH, '<', $filename) or die $!;
-while(<FH>) { print $_; }
-close(FH);
 
+# get user input 
+print {*STDERR} "Enter link: ";
+my $link = <STDIN>;
+
+# download 
+open(my $pipe_fh, "-|", "yt-dlp -t mp4 -o - $link --no-exec");
+
+# write to output as it downloads with seperators
+my $buf = "";
+while(sysread($pipe_fh, $buf, 512) != 0) {
+  print("\r\n--$boundary\nContent-Type: video/mp4\r\n\r\n$buf");
+} print("\r\n--$boundary--"); # finish
